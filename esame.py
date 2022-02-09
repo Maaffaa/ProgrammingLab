3ba1980a0dca040efd9a93f2a2ecb30a5aea0d62
Maf12 <94190129+Maaffaa@users.noreply.github.com>
2022-02-09 18:25:40 +0000

    completo

--- esame.py
+++ esame.py
@@ -283,3 +283,31 @@ def compute_avg_monthly_difference(series,first,last): # calcola la variazione m
 #print(S)
 #s = (compute_avg_monthly_difference(S,'1951','1949'))
 #print(s)
+=======
+
+class MovingAverage():
+    def __init__(self, numero):
+        if isinstance(numero, int):
+            self.num = numero
+        else:
+            raise ExamException('Il numero deve essere un numero intero positivo e deve essere espresso in cifre.')
+    
+    def compute(self,serie):
+        if isinstance(serie, list):
+            if len(serie) >= self.num:
+                risultato = []
+                for i in range(0,len(serie)-(self.num-1)):
+                    risultato.append(media(serie[i:i+self.num]))
+                return risultato
+            else:
+                raise ExamException("La lista deve essere più lunga del numero inserito nell'oggetto MovingAverage.")
+        else:
+            raise ExamException('Per la funzione "compute" bisogna inserire una lista.')
+
+media = lambda serie: sum(serie)/len(serie)    
+mob = MovingAverage(2)
+print(mob.compute([2,4,8,16]))
+print(mob.compute([2]))
+print(mob.compute((5,6,8)))
+print(mob.compute([8.56,4.23,5]))
+>>>>>>> origin/main
